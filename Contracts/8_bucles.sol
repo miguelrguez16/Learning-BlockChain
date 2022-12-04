a019a64249cc2afcdc1d2dd508e00731c73f978e
miguelrguez16 <rodriguezgonzalezmiguel16@gmail.com>
2022-12-04 22:47:30 +0100

    feat: funciones avanzadas, herencia, modifieres, require

--- Contracts/8_bucles.sol
+++ Contracts/8_bucles.sol
@@ -3,34 +3,56 @@ pragma solidity ^0.8.13 .0;
 
 contract LoopsAndConditions {
     // suma de los 10 primeros nuemeros a partir de uno
-    function sum(uint _number) public pure returns (uint)
-    {
-        uint aux_sum = 0;
-        for (uint i = _number; i < _number + 10; i+=1) 
-        {
-
-            aux_sum+=i;
+    function sum(uint256 _number) public pure returns (uint256) {
+        uint256 aux_sum = 0;
+        for (uint256 i = _number; i < _number + 10; i += 1) {
+            aux_sum += i;
         }
         return aux_sum;
     }
 
-
     // suma de los 10 primero numero impares a partir de uno
-    function oddSum(uint256 _base) public pure returns (uint256, uint256, uint256, uint256){
-        uint aux_sum = 0;
-        uint i_odd = 0;
+    function oddSum(uint256 _base)
+        public
+        pure
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        uint256 aux_sum = 0;
+        uint256 i_odd = 0;
 
-        uint tmp = _base + 1;
-        while(i_odd < 10){
-            if(_base % 2 != 0){
+        uint256 tmp = _base + 1;
+        while (i_odd < 10) {
+            if (_base % 2 != 0) {
                 // es impar
-                aux_sum+=_base;
-                i_odd+=1;
+                aux_sum += _base;
+                i_odd += 1;
             }
 
             _base = _base + 1;
         }
         return (_base, aux_sum, i_odd, tmp);
     }
-}
 
+    function sum_rest(
+        string memory typeOperation,
+        uint256 a,
+        uint256 b
+    ) public pure returns (uint256) {
+        bytes32 hashOperation = keccak256(abi.encodePacked(typeOperation));
+        bytes32 hashSuma = keccak256(abi.encodePacked("suma"));
+        bytes32 hashResta = keccak256(abi.encodePacked("resta"));
+
+        if (hashOperation == hashSuma) {
+            return a + b;
+        } else if (hashOperation == hashResta) {
+            return a - b;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ Contracts/FuncionesAvanzadas/2_funciones_avanzadas.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4 .0;
+
+contract Food {
+    // Estructura de datos
+    struct dinnerPlate {
+        string name;
+        string ingredients;
+    }
+
+    // Menu del dia
+    dinnerPlate[] public menu;
+
+    // Creacion de un nuevo menu
+    function newMenu(string memory _name, string memory _ingredients) internal {
+        menu.push(dinnerPlate(_name, _ingredients));
+    }
+}
+
+contract Hamburguer is Food {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Cocinar una hamburguesa desde el Smart Contract principal
+    function doHamburguer(string memory _ingredients, uint256 _units) external {
+        require(_units <= 5, "Ups, no puedes pedir tantas hamburguesas");
+        newMenu("hamburger", _ingredients);
+    }
+
+    // Modifier: permitir el acceso unicamente al owner y solo al owner
+    modifier onlyOwner() {
+        require(
+            owner == msg.sender,
+            "No tienes permisos para ejecutar esta funcion"
+        );
+        _;
+    }
+
+    // Restriccion a la ejecucion (solo puede el owner)
+    function hashPrivateNumber(uint256 _number)
+        public
+        view
+        onlyOwner
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_number));
+    }
+}
