191fec1b1ea9c997688b0971be262ff71dd1ee68
miguelrguez16 <rodriguezgonzalezmiguel16@gmail.com>
2022-12-04 19:28:44 +0100

    feat: fin fundamentos de solidity

---
+++ Contracts/7_maths.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13 .0;
+
+contract Maths {
+    // view obtiene info de la cadena de bloques
+    // ya  estña
+
+    function suma(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function mul(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) public pure returns (uint256) {
+        return a / b;
+    }
+
+    function exp(uint256 a, uint256 b) public pure returns (uint256) {
+        return a**b;
+    }
+
+    function mod(uint256 a, uint256 b) public pure returns (uint256) {
+        return a % b;
+    }
+
+    // (x +y ) mod k
+    function _addmod(
+        uint256 a,
+        uint256 b,
+        uint256 k
+    ) public pure returns (uint256,uint256) {
+        return (addmod(a, b, k), (a+b)%k);
+    }
+
+    // (x +y ) mod k
+    function _mulmod(
+        uint256 a,
+        uint256 b,
+        uint256 k
+    ) public pure returns (uint256,uint256) {
+        return (mulmod(a, b, k), (a*b)%k);
+    }
+}
