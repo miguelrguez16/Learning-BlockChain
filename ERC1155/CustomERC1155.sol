2b13dfa3658ed880b7f13f31eef5a1518b69daf8
miguelrguez16 <rodriguezgonzalezmiguel16@gmail.com>
2022-12-22 14:27:36 +0100

    h

---
+++ ERC1155/CustomERC1155.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13 .0;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC1155/ERC1155.sol";
+
+contract CustomERC1155 is ERC1155{
+    // Situacion
+    // publicacion de unos tokens a una 
+    uint256 
+
+    constructor() ERC1155() {}
+}
