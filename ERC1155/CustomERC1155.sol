da4f1643cc270164011e1d36d0ce549f6bf36a02
Miguel Rodríguez González <uo263494@uniovi.es>
2022-12-22 14:58:51 +0100

    feat: CustomERC1155.sol

--- ERC1155/CustomERC1155.sol
+++ ERC1155/CustomERC1155.sol
@@ -5,8 +5,22 @@ import "@openzeppelin/contracts@4.5.0/token/ERC1155/ERC1155.sol";
 
 contract CustomERC1155 is ERC1155{
     // Situacion
-    // publicacion de unos tokens a una 
-    uint256 
+    // publicacion de unos tokens a una MarketPlace
 
-    constructor() ERC1155() {}
+    uint256 public constant  GOLD_POKEMON = 0;
+    uint256 public constant  SILVER_POKEMON = 1;
+    uint256 public constant  THOR_HAMMER = 2;
+    uint256 public constant  SWORD = 3;
+    uint256 public constant  SHIELD = 4;
+
+
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+        _mint(msg.sender, GOLD_POKEMON, 10**18, "");
+        _mint(msg.sender, SILVER_POKEMON, 10**27, "");
+        _mint(msg.sender, THOR_HAMMER, 1, "");
+        _mint(msg.sender, SWORD, 10**9, "");
+        _mint(msg.sender, SHIELD, 10**8, "");
+    }
+
+
 }
