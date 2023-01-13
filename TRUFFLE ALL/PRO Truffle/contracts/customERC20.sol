afb588afc0017efbe441cee177a03a8eada07e65
Miguel Rodríguez González <uo263494@uniovi.es>
2023-01-13 20:25:57 +0100

    fix: test truffle

--- TRUFFLE ALL/PRO Truffle/contracts/customERC20.sol
+++ TRUFFLE ALL/PRO Truffle/contracts/customERC20.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.4;
 import "./ERC20.sol";
 
 contract customERC20 is ERC20 {
-
     // Constructor del Smart Contract
-    constructor(string memory _name, string memory _symbol) 
-    ERC20(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {}
 
     // Creacion de nuevos Tokens
     function crearTokens() public {
-        _mint(msg.sender, 10**10);
+        _mint(msg.sender, 1000);
     }
 
     // Destruir Tokens
-    function destruirTokens(address _account, uint _amount) public {
+    function destruirTokens(address _account, uint256 _amount) public {
         _burn(_account, _amount);
     }
 }
