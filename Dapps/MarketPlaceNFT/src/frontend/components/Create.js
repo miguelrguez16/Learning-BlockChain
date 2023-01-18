b21dbb7a72a6201a6a62e410b4a5d66189e4eeb0
Miguel Rodríguez González <uo263494@uniovi.es>
2023-01-18 17:40:06 +0100

    feat: components MarketPlace

---
+++ Dapps/MarketPlaceNFT/src/frontend/components/Create.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Row, Form, Button } from "react-bootstrap";
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+const Create = ({ marketplace, nft }) => {
+
+}
+
+export default Create;
