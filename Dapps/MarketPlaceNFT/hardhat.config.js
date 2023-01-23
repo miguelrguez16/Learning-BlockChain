c489824abfce49bd9d78b649d88e18f0a1067b0c
Miguel Rodríguez González <uo263494@uniovi.es>
2023-01-23 22:14:33 +0100

    feat: deploy polygon

--- Dapps/MarketPlaceNFT/hardhat.config.js
+++ Dapps/MarketPlaceNFT/hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   solidity: "0.8.4",
@@ -13,7 +15,16 @@ module.exports = {
     ganache: {
       url: "http://127.0.0.1:7545"
     },
+    polygon: {
+      url: 'https://rpc-mumbai.maticvigil.com/v1/ ',
+      accounts: [privateKey]
+    },
     hardhat: {
     },
+
   },
 };
+
+// npx hardhat run src/backend/scripts/deploy.js --network polygon
+// Deploying contracts with the account: 0xfdF3BE754f88500Dce5A85369E5fF6487Aa8297A
+// Account balance: 700000000000000000
